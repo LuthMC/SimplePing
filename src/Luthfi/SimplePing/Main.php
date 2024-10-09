ffa4c07a3f388ff5c69be38a4f4b1e16198ea9ad
LuthMC <luthfi366822@gmail.com>
2024-10-09 13:35:15 +0700

    Add cooldown

--- src/Luthfi/SimplePing/Main.php
+++ src/Luthfi/SimplePing/Main.php
@@ -6,14 +6,18 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener {
 
     private string $pingMessage;
+    private array $cooldowns = [];
+    private int $pingCooldown;
 
     public function onEnable(): void {
         $this->saveDefaultConfig();
         $this->pingMessage = $this->getConfig()->get("ping-message", "§eYou got pinged!");
+        $this->pingCooldown = $this->getConfig()->get("ping-cooldown", 5);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
@@ -25,9 +29,23 @@ class Main extends PluginBase implements Listener {
         $players = $this->getServer()->getOnlinePlayers();
 
         foreach ($players as $player) {
-            if (strpos($message, '@' . $player->getName()) !== false) {
-                $player->sendTitle($this->pingMessage, $message);
+            if (stripos($message, '@' . $player->getName()) !== false) {
+                if ($this->canPing($player)) {
+                    $player->sendTitle($this->pingMessage, TextFormat::clean($message));
+                    $this->cooldowns[$player->getName()] = time();
+                }
             }
         }
+    }
+
+    /**
+     * Check if a player can be pinged.
+     *
+     * @param Player $player
+     * @return bool
+     */
+    private function canPing(Player $player): bool {
+        $lastPing = $this->cooldowns[$player->getName()] ?? 0;
+        return (time() - $lastPing) >= $this->pingCooldown;
     }
 }
